98307b5e645c9ab2e86e2e6350eefa7fa1b6ec05
baka <a.khaleghi95@gmail.com>
2017-11-01 23:40:36 +0330

    some basics

---
+++ scr/model.py
@@ -0,0 +1,10 @@
+import numpy as np
+import tensorflow as tf
+import sklearn as sc
+
+def main(arg):
+    pass
+
+
+if __name__ == '__main__':
+    main()
---
+++ scr/preprocessing.py
@@ -0,0 +1,9 @@
+import numpy as np
+import tensorflow as tf
+import sklearn as sc
+import pandas as pd
+
+
+
+if __name__ == '__main__':
+    main()
